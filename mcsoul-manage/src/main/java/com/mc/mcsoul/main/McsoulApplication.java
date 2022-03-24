0994b5ee118a8e39f9ffb8db7de7f4d4235c005d
SororTina <719990559@qq.com>
2022-03-25 00:56:41 +0800

    springboot启动类配置修改

--- mcsoul-manage/src/main/java/com/mc/mcsoul/main/McsoulApplication.java
+++ mcsoul-manage/src/main/java/com/mc/mcsoul/main/McsoulApplication.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.mc.mcsoul.dao.mapper")
-@SpringBootApplication
+@MapperScan("com.mc.mcsoul.dao")
+@SpringBootApplication(scanBasePackages = "com.mc.mcsoul")
 public class McsoulApplication {
 
     public static void main(String[] args)   {

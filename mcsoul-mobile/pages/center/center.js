de7bde79f84d831a1857635739b6a221276fc8b3
SororTina <719990559@qq.com>
2022-04-15 15:40:10 +0800

    --

--- mcsoul-mobile/pages/center/center.js
+++ mcsoul-mobile/pages/center/center.js
@@ -33,39 +33,12 @@ Component({
         userInfo: wx.getStorageSync('userInfo')
       })
     },
-    login() {
-      let that = this;
-      wx.login({
-        success(res) {
-          if (res.code) {
-            // 发送 res.code 到后台换取 openId, sessionKey, unionId
-            $post({
-              url: '/sys/manage/login',
-              data: {
-                js_code: res.code,
-              }
-            }).then(res => {
-              if (res.userInfo != undefined && res.userInfo.id !== undefined) {
-                that.setData({
-                  userInfo : res.userInfo,
-                  hasUserInfo: true
-                })
-                wx.setStorageSync('userInfo', res.userInfo);
-              }
-            })
-          } else {
-            console.log('登录失败！' + res.errMsg)
-          }
-        }
-      })
-    },
     getUserInfo() {
       let that = this;
       wx.getUserProfile({
         desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
         success: (res) => {
           that.userInfo = res.userInfo;
-          console.log(res)
           wx.login({
             success(res) {
               if (res.code) {
